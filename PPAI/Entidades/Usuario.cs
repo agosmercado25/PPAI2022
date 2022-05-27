707f03180b090cb62a9d3c65dd31b79a63488fe6
Agostina <agostinamercado1999@gmail.com>
2022-05-27 17:16:03 -0300

    Entidades

---
+++ PPAI/Entidades/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI.Entidades
+{
+    public class Usuario
+    {
+        private string usuario;
+        private string clave;
+
+        public Usuario(string nombreUsuario, string password)
+        {
+            this.usuario = nombreUsuario;
+            this.clave = password;
+        }
+
+        public string NombreDeUsuario
+        {
+            get => usuario;
+            set => usuario = value;
+        }
+
+        public string Password
+        {
+            get => clave;
+            set => clave = value;
+        }
+    }
+}
